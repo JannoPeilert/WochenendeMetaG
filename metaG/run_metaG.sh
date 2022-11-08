b87d82fcb0e0bf15be52fff5cfaf0f34883d1269
JannoPeilert <Peilert.Janno@mh-hannover.de>
2022-11-08 12:37:56 +0100

    first generaly functioning version

--- metaG/run_metaG.sh
+++ metaG/run_metaG.sh
@@ -10,12 +10,14 @@ eval $(parse_yaml $WOCHENENDE_DIR/config.yaml)
 # use SLURM job scheduler (yes, no)
 if [[ "${USE_CUSTOM_SCHED}" == "yes" ]]; then
     #echo USE_CUSTOM_SCHED set"
-    scheduler=$CUSTOM_SCHED_CUSTOM_PARAMS_SINGLECORE
+    scheduler=$CUSTOM_SCHED_CUSTOM_PARAMS       #_SINGLECORE
 fi
 if [[ "${USE_SLURM}" == "yes" ]]; then
     #echo USE_SLURM set"
-    scheduler=$SLURM_CUSTOM_PARAMS_SINGLECORE
+    scheduler=$SLURM_CUSTOM_PARAMS              #_SINGLECORE
 fi
+
+echo "Starting MetaG"
 
 # if growth rate directory is missing, MetaG will not start
 if [[ ! -d "../growth_rate" ]]
@@ -23,10 +25,40 @@ then
     echo "ERROR: Growth rate directory missing. MetaG won't be started"
     exit
 fi
+# if no directories with _subsamples were found, MetaG will not start
+count_subdic=$(ls -1 ../*_subsamples 2>/dev/null | wc -l)
+if [[ -z "$count_subdic" ]]
+then
+  echo "ERROR: No _subsamples directories were found. MetaG won't be started"
+  exit
+fi
 
-# Save output log in directory containing bams and preprocess script
-output_log="plot_"$(date +%s)".log"
-echo "INFO: output_log: " $output_log
+# update path of .csv's in MetaG.properties
+#SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) # returns path of this script
+
+# enter growth_rate directory
+cd ../growth_rate/ #2>/dev/null
+
+# primary loop for executing MetaG, starts it for every subdirectory
+for d in *_subsamples/; do
+    cd ./"$d"
+    pwd_dir=$(pwd)
+
+    # copys MetaG.properties to current folder
+    cp ../../metaG/image/bin/MetaG.properties .
+
+    # changes path in MetaG.properties to current folder
+    sed -i "4s#.*#DIRECTORY=$pwd_dir#" ./MetaG.properties
+
+    #head -4 ./MetaG.properties|tail -1 # gives line with directory path
+
+    # executes MetaG
+    bash ../../metaG/image/bin/MetaG
+
+    cd ..
+    echo
+done
+
 
 echo "Finished  MetaG"
 
